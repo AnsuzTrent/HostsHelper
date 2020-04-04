8c0a10fb311e713e83fbb0271ee7317d7826ce5a
Fehu <wd_91209@sina.com>
2020-04-04 20:30:41 +0800

    目前基本可以投入使用 修改：  1. 取消kotlin 预期：  1. 试图增加失败回收

--- src/main/java/org/apache/fraud/search/rules/ChinaZ.java
+++ src/main/java/org/apache/fraud/search/rules/ChinaZ.java
@@ -5,6 +5,7 @@
 
 package org.apache.fraud.search.rules;
 
+import org.apache.fraud.search.rules.base.BaseParser;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
@@ -20,11 +21,12 @@ public class ChinaZ extends BaseParser {
 		super(site);
 	}
 
-
 	@Override
-	public Vector<String> getResult() {
+	protected Vector<String> getResult() {
 		String url = "http://tool.chinaz.com/dns?type=1&host=" + site + "&ip=";
 		Vector<String> recode = new Vector<>();
+		Vector<String> noResult = new Vector<>();
+		noResult.add("none\n");
 
 		try {
 			Document doc = getDocumentFromPage(url);
@@ -32,22 +34,22 @@ public class ChinaZ extends BaseParser {
 			String host = doc.getElementById("host").attr("value");
 
 			// 包括IP, "-", 其它奇怪的东西，需要考虑换正则式
-			String[] IPTmp = doc.getElementsByClass("w60-0 tl").text()
+			String[] ipTmp = doc.getElementsByClass("w60-0 tl").text()
 					.split("\\[.*?]");
 
-			String[] IP = new String[IPTmp.length];
+			String[] ip = new String[ipTmp.length];
 
 			int i = 0, j = 0;
-			while (i < IPTmp.length) {
-				IPTmp[i] = IPTmp[i].replaceAll("([ \\-]|\\.\\.+)", "");
-				if ("".equals(IPTmp[i])) {
+			while (i < ipTmp.length) {
+				ipTmp[i] = ipTmp[i].replaceAll("([ \\-]|\\.\\.+)", "");
+				if ("".equals(ipTmp[i])) {
 					i++;
 					continue;
 				}
-				IP[j++] = IPTmp[i++];
+				ip[j++] = ipTmp[i++];
 			}
 
-			for (String s : IP) {
+			for (String s : ip) {
 				if (s != null) {
 					if (recode.indexOf("\n" + s + " " + host) == -1 & !"-".equals(s)) {
 						recode.addElement("\n" + s + " " + host);
@@ -58,16 +60,19 @@ public class ChinaZ extends BaseParser {
 			Collections.sort(recode);
 
 		} catch (IOException e) {
-//			e.printStackTrace();
-			return null;
+			// 可能联网超时
+			printToUserInterface("\nError in [" + e.getMessage() + "]\n Of the \"" + site + "\"");
+			noResult.add(site);
+			return noResult;
 		}
 
 		// 取得结果是否为空
 		if (!recode.isEmpty()) {
 			recode.addElement("\n");
 		} else {
-//			appendString("输入的网址没有找到对应ip");
-			return null;
+			printToUserInterface("\n输入的网址:" + site + " 没有找到对应ip");
+			noResult.add(site);
+			return noResult;
 		}
 
 		return recode;
